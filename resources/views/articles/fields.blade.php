b9695f4196fe030f17b4192b36f36ab41ddd6c6c
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-12-15 13:47:48 +0100

    Adding gest error add article

--- resources/views/articles/fields.blade.php
+++ resources/views/articles/fields.blade.php
@@ -1,9 +1,15 @@
 <label for="title">title</label>
 <input class="form-control" type="text" id="title" name="title" required value="{{$article->title}}">
-<br>
+@error('title')
+<div class="alert alert-danger">{{ $message }}</div>
+@enderror
+
 <label for="body">body</label>
 <textarea class="form-control" type="text" id="body" name="body" required>{{$article->body}}</textarea>
-<br>
+@error('body')
+<div class="alert alert-danger">{{ $message }}</div>
+@enderror
+
 <label for="category_id">category</label>
 <select class="form-control" id="category_id" name="category_id">
     <option>1</option>
@@ -12,4 +18,7 @@
     <option>4</option>
     <option>5</option>
 </select>
+@error('category_id')
+<div class="alert alert-danger">{{ $message }}</div>
+@enderror
 <input type="submit" value="send">
